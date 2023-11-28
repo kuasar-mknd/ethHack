4dcaaa383ab68a19b9f6569c07d6d0223d65d0a2
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2023-11-28 14:58:24 +0000

    wip

--- .devcontainer/create-ethhack-database.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS ethhack;
-    GRANT ALL PRIVILEGES ON \`ethhack%\`.* TO '$MYSQL_USER'@'%';
-EOSQL
