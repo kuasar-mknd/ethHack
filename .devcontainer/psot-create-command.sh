bcf202fea07d197213993fdd4770f3159f5145c3
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2023-12-16 22:11:27 +0100

    wip - reset all

--- .devcontainer/psot-create-command.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Démarrer Sail et effectuer des actions post-démarrage
-/var/www/html/vendor/bin/sail up -d
-/var/www/html/vendor/bin/sail artisan key:generate
-/var/www/html/vendor/bin/sail artisan migrate
